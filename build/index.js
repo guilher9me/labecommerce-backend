3c702bcbc32650af35e22a0cfdfa7df5294fa27f
Guilherme <guibarros.dev@gmail.com>
2023-06-28 15:07:31 -0300

    typescript-ii

--- build/index.js
+++ build/index.js
@@ -3,4 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
 console.log(database_1.users);
 console.log(database_1.products);
+(0, database_1.createUser)("user03", "Zac Farro", "zacfarro@emo.com", "paramore");
+console.table((0, database_1.getAllUsers)());
+(0, database_1.createProducts)("prod003", "Pedalboard", 3500.0, "The best of pedalboards", "https://images.unsplash.com/photo-1565052168411-f958235a7972?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80");
+console.table((0, database_1.getAllProducts)());
+console.table((0, database_1.searchProductsByName)("pro mic"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import { users, products } from "./database";
+import {
+  users,
+  products,
+  createUser,
+  getAllUsers,
+  createProducts,
+  getAllProducts,
+  searchProductsByName,
+} from "./database";
 console.log(users);
 console.log(products);
+
+createUser("user03", "Zac Farro", "zacfarro@emo.com", "paramore");
+console.table(getAllUsers());
+
+createProducts(
+  "prod003",
+  "Pedalboard",
+  3500.0,
+  "The best of pedalboards",
+  "https://images.unsplash.com/photo-1565052168411-f958235a7972?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+);
+console.table(getAllProducts());
+console.table(searchProductsByName("pro mic"));
